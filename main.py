312b285b1f2e6f19b5998067929f05e1ad5e7ee9
Sadie Levinson-Mader <samlevinson.nmlgroup@gmail.com>
2026-04-05 16:15:00 -0400

    Added /tasks endpoint returning empty list

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+@app.get("/tasks")
+def list_tasks():
+	return {"tasks": []}
+
 @app.get("/")
 def root():
 	return {"message": "Hello from The Festiva Project Task Tracker."}
